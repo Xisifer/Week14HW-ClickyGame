abeb9c5f3353c2041cbce3be02bec6453af9aca5
Scott Finlay <xisifer@gmail.com>
2019-09-02 17:48:07 -0500

    PSeudocoded out some logic, currently trying to fiddle around with OnClick functions and getting everything up and working

--- clicky/src/App.js
+++ clicky/src/App.js
@@ -1,31 +1,78 @@
-import React from 'react';
-import logo from './logo.svg';
-
-import CharCard from "./components/CharCard";
+import React, { Component } from "react";
+import CharCard from "./components/CharCard/CharCard.js"
 import Wrapper from "./components/Wrapper";
-import friends from "./friends.json";
+import Title from "./components/Title";
+import chars from "./chars.json";
 
-import './App.css';
+class App extends Component {
+  // Setting this.state.chars to the chars json array
+  state = {
+    chars
+  };
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+  removeChar = id => {
+    // Filter this.state.chars for chars with an id not equal to the id being removed
+    const chars = this.state.chars.filter(char => char.id !== id);
+    // Set this.state.chars equal to the new chars array
+    this.setState({ chars });
+  }
+
+  // Simple test function to see whether functions are being called successfully via the OnClick
+  alerter() {
+    alert("FUNCTION CALLED SUCCESSFULLY");
+  }
+
+  // clickChecker() {
+  //   // Pseudocode Time!!:
+  //   const score = 0;
+  //   const highScore = 0;
+  //   // When the image is clicked, we need to...
+    
+  //   // Create an array storing the ID of all clicked characters
+  //   const clickedChars = [];
+  //   // Check whether or not the character's id is located in the clickedChars array (aka, it has already been clicked)
+  //   // If it HAS been clicked before, then...
+      
+  //   //   Reset Score back to 0
+  //   score = 0;
+  //   //   Shake the screen?
+  //   //   Change the text to say the user guessed wrong
+  //   //   Clear the clickedChars array
+  //   // If it HAS NOT been clicked before, then...
+  //   //   Add the current char into the "memory" of which chars have or have not been clicked
+  //   //   Increase Score by 1
+  //   //   Check whether this increase will be greater than the current Top Score
+  //   //     If yes, then increase Top Score
+  //   //     If no.....do nothing?
+  //   //   Change the text to say that the user guessed right
+  //   // After checking those, then...
+  //   //   Randomize/re-shuffle the position of the character cards
+    
+  // }
+
+
+
+
+
+  // Map over this.state.chars and render a CharCard component for each char object
+  render() {
+    return (
+      <Wrapper>
+        <Title>chars List</Title>
+        {this.state.chars.map(char => (
+          <CharCard
+            removeChar={this.removeChar}
+            id={char.id}
+            key={char.id}
+            name={char.name}
+            image={char.image}
+            occupation={char.occupation}
+            location={char.location}
+          />
+        ))}
+      </Wrapper>
+    );
+  }
 }
 
 export default App;
--- clicky/src/components/CharCard/CharCard.js
+++ clicky/src/components/CharCard/CharCard.js
@@ -1,13 +1,19 @@
 import React from "react";
 import "./style.css";
 
-function FriendCard(props) {
+function CharCard(props) {
   return (
+    
+
+
+    <React.Fragment>
     <div className="card">
+
       <div className="img-container">
         <img alt={props.name} src={props.image} />
       </div>
-      <div className="content">
+
+      {/* <div className="content">
         <ul>
           <li>
             <strong>Name:</strong> {props.name}
@@ -18,11 +24,13 @@ function FriendCard(props) {
           <li>
             <strong>Address:</strong> {props.location}
           </li>
-        </ul>
-      </div>
-      <span onClick={props.removeFriend} className="remove">𝘅</span>
+        </ul>        
+      </div> */}
+      {/* Inserting props.id as an argument for the RemoveChar function, so we can specifically tell it which ID to be removing, i.e. the thing we just clicked on */}
+      <span onClick={() => props.removeChar(props.id)} className="remove">𝘅</span>
     </div>
+  </React.Fragment>
   );
 }
 
-export default FriendCard;
+export default CharCard;
